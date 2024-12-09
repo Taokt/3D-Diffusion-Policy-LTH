69a2543a25722c0fac39395feab0235755dc4ac6
Han Fu <fuhan0148@gmail.com>
2024-12-09 20:44:40 +0100

    generate fake data

---
+++ scripts/gen_demonstration_isaaclab.sh
@@ -0,0 +1,12 @@
+# bash scripts/gen_demonstration_isaaclab.sh None
+
+cd third_party/IsaacLab/src
+
+task=${1}
+
+CUDA_VISIBLE_DEVICES=0 python gen_demonstration_expert.py --env_name $task \
+                        --num_episodes 10 \
+                        --root_dir "../../../3D-Diffusion-Policy/data/" \
+                        --img_size 84 \
+                        --not_use_multi_view \
+                        --use_point_crop
